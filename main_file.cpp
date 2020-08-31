6f69e65dbfae6b3e4155c53acff65245d5fc841f
Albert <szulcalbert@gmail.com>
2020-08-31 19:01:24 +0200

    Added: OBJ files reading, 1. version of rocket movement

--- main_file.cpp
+++ main_file.cpp
@@ -28,19 +28,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "shaderprogram.h"
 #include "myCube.h"
 #include "myTeapot.h"
+#include "modelStruct.h"
+#include "loadOBJ.hpp"
 
-float speed_x=0; //angular speed in radians
-float speed_y=0; //angular speed in radians
-float aspectRatio=1;
-ShaderProgram *sp; //Pointer to the shader program
-
+float speed_x = 0; //speed in radians
+float speed_y = 0; //angular speed in radians
+float speed_z = 0; //angular speed in radians
+float aspectRatio = 1;
+ShaderProgram* sp; //Pointer to the shader program
+Model spaceCraftModel;
+GLuint tex0;
+GLuint tex1;
 
 //Uncomment to draw a cube
-float* vertices=myCubeVertices;
-float* texCoords= myCubeTexCoords;
-float* colors = myCubeColors;
-float* normals = myCubeNormals;
-int vertexCount = myCubeVertexCount;
+float* vertices;
+float* texCoords;
+float* normals;
+int vertexCount;
+
+//Uncomment to draw a cube
+//float* vertices=myCubeVertices;
+//float* texCoords= myCubeTexCoords;
+//float* colors = myCubeColors;
+//float* normals = myCubeNormals;
+//int vertexCount = myCubeVertexCount;
 
 //Uncomment to draw a teapot
 //float* vertices = myTeapotVertices;
@@ -49,33 +60,35 @@ int vertexCount = myCubeVertexCount;
 //float* normals = myTeapotVertexNormals;
 //int vertexCount = myTeapotVertexCount;
 
-GLuint tex0;
-GLuint tex1;
 
 //Error processing callback procedure
 void error_callback(int error, const char* description) {
 	fputs(description, stderr);
 }
 
-void keyCallback(GLFWwindow* window,int key,int scancode,int action,int mods) {
-    if (action==GLFW_PRESS) {
-        if (key==GLFW_KEY_LEFT) speed_x=-PI/2;
-        if (key==GLFW_KEY_RIGHT) speed_x=PI/2;
-        if (key==GLFW_KEY_UP) speed_y=PI/2;
-        if (key==GLFW_KEY_DOWN) speed_y=-PI/2;
-    }
-    if (action==GLFW_RELEASE) {
-        if (key==GLFW_KEY_LEFT) speed_x=0;
-        if (key==GLFW_KEY_RIGHT) speed_x=0;
-        if (key==GLFW_KEY_UP) speed_y=0;
-        if (key==GLFW_KEY_DOWN) speed_y=0;
-    }
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (action == GLFW_PRESS) {
+		if (key == GLFW_KEY_LEFT) speed_x = PI;
+		if (key == GLFW_KEY_RIGHT) speed_x = -PI;
+		if (key == GLFW_KEY_UP) speed_y = -PI;
+		if (key == GLFW_KEY_DOWN) speed_y = PI;
+		if (key == GLFW_KEY_W) speed_z = 10.0f;
+		if (key == GLFW_KEY_S) speed_z = -10.0f;
+	}
+	if (action == GLFW_RELEASE) {
+		if (key == GLFW_KEY_LEFT) speed_x = 0;
+		if (key == GLFW_KEY_RIGHT) speed_x = 0;
+		if (key == GLFW_KEY_UP) speed_y = 0;
+		if (key == GLFW_KEY_DOWN) speed_y = 0;
+		if (key == GLFW_KEY_W) speed_z = 0;
+		if (key == GLFW_KEY_S) speed_z = 0;
+	}
 }
 
-void windowResizeCallback(GLFWwindow* window,int width,int height) {
-    if (height==0) return;
-    aspectRatio=(float)width/(float)height;
-    glViewport(0,0,width,height);
+void windowResizeCallback(GLFWwindow* window, int width, int height) {
+	if (height == 0) return;
+	aspectRatio = (float)width / (float)height;
+	glViewport(0, 0, width, height);
 }
 
 GLuint readTexture(const char* filename) {
@@ -104,13 +117,20 @@ GLuint readTexture(const char* filename) {
 //Initialization code procedure
 void initOpenGLProgram(GLFWwindow* window) {
 	//************Place any code here that needs to be executed once, at the program start************
-	glClearColor(0,0,0,1);
+	glClearColor(0, 0, 0, 1);
 	glEnable(GL_DEPTH_TEST);
-	glfwSetWindowSizeCallback(window,windowResizeCallback);
-	glfwSetKeyCallback(window,keyCallback);
-	sp=new ShaderProgram("v_simplest.glsl",NULL,"f_simplest.glsl");
-	tex0 = readTexture("metal.png");
-	tex1 = readTexture("metal_spec.png");
+	glfwSetWindowSizeCallback(window, windowResizeCallback);
+	glfwSetKeyCallback(window, keyCallback);
+	sp = new ShaderProgram("v_simplest.glsl", NULL, "f_simplest.glsl");
+	spaceCraftModel = loadOBJ("models/longBlue/longBlue.obj");
+	//spaceCraftModel = loadOBJ("models/futuristic/futuristic.obj");
+	tex0 = readTexture("models/longBlue/longBlue.png");
+	//tex0 = readTexture("models/futuristic/AircraftS.png");
+
+	vertices = &spaceCraftModel.verticies[0];
+	texCoords = &spaceCraftModel.texCoords[0];
+	normals = &spaceCraftModel.vertexNormals[0];
+	vertexCount = spaceCraftModel.vertexCount;
 }
 
 //Release resources allocated by the program
@@ -120,52 +140,54 @@ void freeOpenGLProgram(GLFWwindow* window) {
 }
 
 //Drawing procedure
-void drawScene(GLFWwindow* window,float angle_x,float angle_y) {
+void drawScene(GLFWwindow* window, float distance_z, float angle_x, float angle_y) {
 	//************Place any code here that draws something inside the window******************l
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glm::mat4 V=glm::lookAt(
-        glm::vec3(0.0f,0.0f,-4.0f),
-        glm::vec3(0.0f,0.0f,0.0f),
-        glm::vec3(0.0f,1.0f,0.0f)); //compute view matrix
-    glm::mat4 P=glm::perspective(50.0f*PI/180.0f, 1.0f, 1.0f, 50.0f); //compute projection matrix
+	glm::mat4 V = glm::lookAt(
+		glm::vec3(0.0f, 0.0f, -20.0f), // camera located at (x, y, z)
+		glm::vec3(0.0f, 0.0f, 0.0f), // looking at (x, y, z)
+		glm::vec3(0.0f, 1.0f, 0.0f)); // up vector
+	glm::mat4 P = glm::perspective(60.0f * PI / 180.0f, 1.0f, 1.0f, 100.0f); //compute projection matrix
 
-    sp->use();//activate shading program
-    //Send parameters to graphics card
-    glUniformMatrix4fv(sp->u("P"),1,false,glm::value_ptr(P));
-    glUniformMatrix4fv(sp->u("V"),1,false,glm::value_ptr(V));
-
-    glm::mat4 M=glm::mat4(1.0f);
-	M=glm::rotate(M,angle_y,glm::vec3(1.0f,0.0f,0.0f)); //Compute model matrix
-	M=glm::rotate(M,angle_x,glm::vec3(0.0f,1.0f,0.0f)); //Compute model matrix
-    glUniformMatrix4fv(sp->u("M"),1,false,glm::value_ptr(M));
+	sp->use();//activate shading program
+	//Send parameters to graphics card
+	glUniformMatrix4fv(sp->u("P"), 1, false, glm::value_ptr(P));
+	glUniformMatrix4fv(sp->u("V"), 1, false, glm::value_ptr(V));
 
 
-    glEnableVertexAttribArray(sp->a("vertex")); //Enable sending data to the attribute vertex
-    glVertexAttribPointer(sp->a("vertex"),4,GL_FLOAT,false,0,vertices); //Specify source of the data for the attribute vertex
+	//M = glm::translate(M, glm::vec3(0.0f, -10.0f, 0.0f));
+	glm::mat4 M = glm::mat4(1.0f);
+	M = glm::translate(M, glm::vec3(0.0f, -10.0f, distance_z + 20.0f));
 
-	glEnableVertexAttribArray(sp->a("texCoord0")); //Enable sending data to the attribute texCoord0
-	glVertexAttribPointer(sp->a("texCoord0"), 2, GL_FLOAT, false, 0, texCoords); //Specify source of the data for the attribute texCoord0
+	M = glm::rotate(M, angle_x + PI, glm::vec3(0.0f, 1.0f, 0.0f));
+	M = glm::rotate(M, angle_y - PI / 3, glm::vec3(1.0f, 0.0f, 0.0f));
 
-	glEnableVertexAttribArray(sp->a("normal")); //Enable sending data to the attribute color
-	glVertexAttribPointer(sp->a("normal"), 4, GL_FLOAT, false, 0, normals); //Specify source of the data for the attribute normal
 
-	glUniform1i(sp->u("textureMap0"), 0); // Associate sampler textureMap0 with the 0-th texturing unit
-	glActiveTexture(GL_TEXTURE0); //Assign texture tex0 to the 0-th texturing unit
+	M = glm::scale(M, glm::vec3(0.003f, 0.003f, 0.003f));
+	glUniformMatrix4fv(sp->u("M"), 1, false, glm::value_ptr(M));
+
+	glEnableVertexAttribArray(sp->a("vertex")); //Enable sending data to the attribute vertex
+	glVertexAttribPointer(sp->a("vertex"), 4, GL_FLOAT, false, 0, vertices); //Specify source of the data for the attribute vertex
+
+	glEnableVertexAttribArray(sp->a("normal")); //Enable sending data to the attribute vertex
+	glVertexAttribPointer(sp->a("normal"), 4, GL_FLOAT, false, 0, normals); //Specify source of the data for the attribute vertex
+
+	glEnableVertexAttribArray(sp->a("texCoord0")); //Enable sending data to the attribute color
+	glVertexAttribPointer(sp->a("texCoord0"), 2, GL_FLOAT, false, 0, texCoords); //Specify source of the data for the attribute color
+
+	glUniform1i(sp->u("textureMap0"), 0);
+	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, tex0);
 
-	glUniform1i(sp->u("textureMap1"), 1); // Associate sampler textureMap1 with the 1-st texturing unit
-	glActiveTexture(GL_TEXTURE1); //Assign texture tex1 to the 1-st texturing unit
-	glBindTexture(GL_TEXTURE_2D, tex1);
+	glDrawArrays(GL_TRIANGLES, 0, vertexCount); //Draw the object
 
-    glDrawArrays(GL_TRIANGLES,0,vertexCount); //Draw the object
-
-    glDisableVertexAttribArray(sp->a("vertex")); //Disable sending data to the attribute vertex
-	glDisableVertexAttribArray(sp->a("texCoord0")); //Disable sending data to the attribute texCoord0
+	glDisableVertexAttribArray(sp->a("vertex")); //Disable sending data to the attribute vertex
 	glDisableVertexAttribArray(sp->a("normal")); //Disable sending data to the attribute normal
+	glDisableVertexAttribArray(sp->a("texCoord0")); //Disable sending data to the attribute texCoord0
 
-    glfwSwapBuffers(window); //Copy back buffer to front buffer
+	glfwSwapBuffers(window); //Copy back buffer to front buffer
 }
 
 int main(void)
@@ -179,7 +201,7 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	window = glfwCreateWindow(500, 500, "OpenGL", NULL, NULL);  //Create a window 500pxx500px titled "OpenGL" and an OpenGL context associated with it.
+	window = glfwCreateWindow(1000, 1000, "OpenGL", NULL, NULL);  //Create a window 500pxx500px titled "OpenGL" and an OpenGL context associated with it.
 
 	if (!window) //If no window is opened then close the program
 	{
@@ -191,7 +213,7 @@ int main(void)
 	glfwSwapInterval(1); //During vsync wait for the first refresh
 
 	GLenum err;
-	if ((err=glewInit()) != GLEW_OK) { //Initialize GLEW library
+	if ((err = glewInit()) != GLEW_OK) { //Initialize GLEW library
 		fprintf(stderr, "Can't initialize GLEW: %s\n", glewGetErrorString(err));
 		exit(EXIT_FAILURE);
 	}
@@ -199,16 +221,18 @@ int main(void)
 	initOpenGLProgram(window); //Call initialization procedure
 
 
-	float angle_x=0; //current rotation angle of the object, x axis
-	float angle_y=0; //current rotation angle of the object, y axis
+	float distance_z = 0; //current rotation position of the object, z axis
+	float angle_x = 0; //current rotation angle of the object, x axis
+	float angle_y = 0; //current rotation angle of the object, y axis
 	glfwSetTime(0); //Zero the timer
 	//Main application loop
 	while (!glfwWindowShouldClose(window)) //As long as the window shouldnt be closed yet...
 	{
-        angle_x+=speed_x*glfwGetTime(); //Add angle by which the object was rotated in the previous iteration
-		angle_y+=speed_y*glfwGetTime(); //Add angle by which the object was rotated in the previous iteration
-        glfwSetTime(0); //Zero the timer
-		drawScene(window,angle_x,angle_y); //Execute drawing procedure
+		distance_z += speed_z * glfwGetTime(); //Add angle by which the object was rotated in the previous iteration
+		angle_x += speed_x * glfwGetTime(); //Add angle by which the object was rotated in the previous iteration
+		angle_y += speed_y * glfwGetTime(); //Add angle by which the object was rotated in the previous iteration
+		glfwSetTime(0); //Zero the timer
+		drawScene(window, distance_z, angle_x, angle_y); //Execute drawing procedure
 		glfwPollEvents(); //Process callback procedures corresponding to the events that took place up to now
 	}
 	freeOpenGLProgram(window);
--- v_simplest.glsl
+++ v_simplest.glsl
@@ -6,23 +6,23 @@ uniform mat4 V;
 uniform mat4 M;
 
 //Attributes
-in vec4 vertex; //Vertex coordinates in model space
-in vec4 normal; //Vertex normal in model space
+in vec4 vertex;
+in vec4 normal;
 in vec2 texCoord0;
 
-//Varying variables
 out vec4 l;
 out vec4 n;
 out vec4 v;
 out vec2 iTexCoord0;
 
+//Varying variables
+
 void main(void) {
-    vec4 lp = vec4(0, 0, -6, 1); //light position, world space
-    l = normalize(V * lp - V * M * vertex); //vector towards the light in eye space
-    v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //vector towards the viewer in eye space
-    n = normalize(V * M * normal); //normal vector in eye space
+    vec4 lp = vec4(0, 0, -6, 1); // place of light
+    l = normalize(V * lp - V * M * vertex); // vector towards light source in eye space
+    v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); // vector towards observer in eye space
+    n = normalize(V * M * normal); // normal vector in eye space
 
     iTexCoord0 = texCoord0;
-
     gl_Position = P * V * M * vertex;
 }
